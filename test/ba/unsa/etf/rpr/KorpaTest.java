3256c734cfdae62e4d98a469f06d3c9bc64cce29
Harun Alagic <halagic1@etf.unsa.ba>
2020-11-03 17:21:57 +0100

    Dodana dva testa za metode klase Korpa

---
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KorpaTest {
+
+    private static Korpa korpa;
+    private static Artikl artikl;
+
+/*    @BeforeEach
+    private static void unosArtikalaUKorpu(){
+        private static korpa = new Korpa();
+        private static artikl = new Artikl("auto", 123, "1");
+    }*/
+
+    @Test
+    public void testIzbacivanjaIzKorpe(){
+        korpa = new Korpa();
+        korpa.dodajArtikl(new Artikl("auto", 123, "1"));
+        //korpa.dodajArtikl(new Artikl("auto", 123, "2"));
+        //korpa.dodajArtikl(new Artikl("auto", 123, "3"));
+        artikl = korpa.izbaciArtiklSaKodom("1");
+
+        assertNotNull(artikl);
+    }
+
+    @Test
+    public void testOdredjivanjaCijene(){
+        korpa = new Korpa();
+        korpa.dodajArtikl(new Artikl("auto", 123, "1"));
+        korpa.dodajArtikl(new Artikl("auto", 123, "2"));
+        korpa.dodajArtikl(new Artikl("auto", 123, "3"));
+        assertEquals(369, korpa.dajUkupnuCijenuArtikala());
+    }
+
+}
